66c843f7c3bbb24adafa5750f6b15d0bc38b4326
Kevin Zhang <kevinzhang@Kevins-M1ProBook.local>
2025-12-27 16:19:34 -0800

    add initial selectors

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
+<script>
+export default {
+  data: () => {
+    return {
+      category: "",
+    };
+  },
+  methods: {
+    setCategory(cat) {
+      this.category = cat;
+    },
+  },
+  mounted() {
+    this.increment();
+  },
+};
+</script>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ category }}</p>
+    <button @click="setCategory('tennis')">Tennis</button>
+    <button @click="setCategory('clash_royale')">Clash Royale</button>
+    <button @click="setCategory('ucla')">UCLA</button>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -24,5 +33,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border: 2px solid #008cba;
+}
+
+button:hover {
+  background-color: #008cba;
+  color: white;
 }
 </style>
