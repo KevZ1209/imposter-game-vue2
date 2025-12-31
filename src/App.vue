1ec3a1596ad00a5ea446217fd90fea26d9c8c972
Kevin Zhang <kevinzhang@Kevins-M1ProBook.local>
2025-12-30 21:21:18 -0800

    move styles to global.css, add CustomButton component with custom event

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script>
+import CustomButton from './components/CustomButton.vue'
+
 export default {
-  data: () => {
-    return {
-      category: "",
-    };
-  },
+  components: { CustomButton },
+  data: () => ({
+    category: "",
+  }),
   methods: {
     setCategory(e) {
       this.category = e.target.value;
-    },
-  },
-  mounted() {
-    this.increment();
+    }
   },
 };
 </script>
@@ -19,38 +17,11 @@ export default {
 <template>
   <div id="app">
     <p>{{ category }}</p>
-    <button @click="setCategory" value="tennis">Tennis</button>
-    <button @click="setCategory" value="clash_royale">Clash Royale</button>
-    <button @click="setCategory" value="ucla">UCLA</button>
+    <h1>Select a Category:</h1>
+    <CustomButton buttonVal="tennis" buttonLabel="Tennis" @button-clicked="setCategory"/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-button {
-  background-color: white;
-  border: none;
-  color: black;
-  padding: 16px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-  border: 2px solid #008cba;
-}
-
-button:hover {
-  background-color: #008cba;
-  color: white;
-}
+/* App styles moved to src/assets/global.css */
 </style>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="setCategory" :value="buttonVal">{{ buttonLabel }}</button>
+</template>
+
+<script>
+export default {
+  props: ["buttonVal", "buttonLabel"],
+  methods: {
+    setCategory(e) {
+      this.$emit('button-clicked', e);
+    }
+  },
+};
+</script>
+
+<style>
+button {
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: inherit;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border: 2px solid #008cba;
+}
+button:hover {
+  background-color: #008cba;
+  color: white;
+}
+</style>
